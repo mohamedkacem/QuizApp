c0c60d66b5903cdd20fbde6cf68c93bef43d6e91
mohamedkacem <kassem.ferchichi@gmail.com>
2021-01-28 22:58:46 +0100

    dddd

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + './quizApp/'));
+app.use(express.static(__dirname + './quizApp'));
 
 app.get('/*', function(req,res) {
     
