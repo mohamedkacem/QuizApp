70f8b59f59ecabe0a2f69494263f667e7cf427c3
mohamedkacem <kassem.ferchichi@gmail.com>
2021-01-28 23:28:20 +0100

    hhh

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + './dist/quizApp/'));
+app.use(express.static(__dirname + './dist/quizApp'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'dist/quizApp'));
+res.sendFile(path.join(__dirname+'/dist/quizApp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
