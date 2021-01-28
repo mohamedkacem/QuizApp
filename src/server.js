18ce32fe17ca81fd811325fe09b31091646c2398
mohamedkacem <kassem.ferchichi@gmail.com>
2021-01-28 23:22:30 +0100

    tawa

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + './dist/quizApp'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'dist/quizApp/index.html'));
+res.sendFile(path.join(__dirname+'/dist/quizApp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
