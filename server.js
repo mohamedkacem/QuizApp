f4d962f79a10539799e8ba9c69b5ccdc88c34dcc
mohamedkacem <kassem.ferchichi@gmail.com>
2021-01-28 23:37:13 +0100

    jj

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ const app = express();
 app.use(express.static(__dirname + './dist/quizApp'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/quizApp/index.html'));
+res.sendFile('index.html', {root: '/dist/quizApp/'}),
 });
 
 // Start the app by listening on the default Heroku port
