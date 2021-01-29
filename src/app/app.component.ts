c24b28d733877046d7a1a2b3a01aaa931441a485
mohamedkacem <kassem.ferchichi@gmail.com>
2021-01-29 19:23:27 +0100

    table ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,17 @@ import { MatModule } from 'src/app/mat/mat.module';
 
 
 
-
 // project_schema
 export interface project_schema {
  
-  author: string;
+  id: number;
   //configuration : Array<any>
-  date: string;
+  name: string;
   //id: string;
-  projectname: string;
+  timestamp: number;
   //reusable: Boolean;
-  type: string;
+  score: number;
+  NombreQ : number
   
 }
 
@@ -27,11 +27,112 @@ export interface project_schema {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent  implements AfterViewInit{
+ 
   title = 'quizApp';
-  displayedColumns: string[] = [ 'author', 'date','projectname', 'type','actions']; // table Column
-  public dataSource : project_schema[] = []; 
+  displayedColumns: string[] = [ 'id', 'name','timestamp', 'score','NombreQ']; // table Column
+  public dataSource : project_schema[] = [
+    {
+      "id": 1,
+      "name": "kacem",
+      "timestamp": 1611942480,
+      "score": 24,
+      "NombreQ": 31
+    },
+    {
+      "id": 2,
+      "name": "mohamed",
+      "timestamp": 1611942420,
+      "score": 3,
+      "NombreQ": 16
+    },
+    {
+      "id": 3,
+      "name": "Atef",
+      "timestamp": 1611942360,
+      "score": 45,
+      "NombreQ": 15
+    },
+    {
+      "id": 4,
+      "name": "mounir",
+      "timestamp": 1611942300,
+      "score": 59,
+      "NombreQ": 74
+    },
+    {
+      "id": 5,
+      "name": "bechir",
+      "timestamp": 1611942240,
+      "score": 17,
+      "NombreQ": 91
+    },
+    {
+      "id": 6,
+      "name": "nada",
+      "timestamp": 1611942180,
+      "score": 85,
+      "NombreQ": 96
+    },
+    {
+      "id": 7,
+      "name": "nouha",
+      "timestamp": 1611942120,
+      "score": 87,
+      "NombreQ": 16
+    },
+    {
+      "id": 8,
+      "name": "name 8",
+      "timestamp": 1611942060,
+      "score": 8,
+      "NombreQ": 41
+    },
+    {
+      "id": 9,
+      "name": "name 9",
+      "timestamp": 1611942000,
+      "score": 98,
+      "NombreQ": 64
+    },
+    {
+      "id": 10,
+      "name": "name 10",
+      "timestamp": 1611941940,
+      "score": 87,
+      "NombreQ": 78
+    },
+    {
+      "id": 11,
+      "name": "name 11",
+      "timestamp": 1611941880,
+      "score": 65,
+      "NombreQ": 81
+    },
+    {
+      "id": 12,
+      "name": "name 12",
+      "timestamp": 1611941820,
+      "score": 8,
+      "NombreQ": 55
+    },
+    {
+      "id": 13,
+      "name": "name 13",
+      "timestamp": 1611941760,
+      "score": 82,
+      "NombreQ": 54
+    },
+    {
+      "id": 14,
+      "name": "name 14",
+      "timestamp": 1611941700,
+      "score": 80,
+      "NombreQ": 54
+    }
+  ]; 
 
 
   ngAfterViewInit() {
+   
   }
 }
